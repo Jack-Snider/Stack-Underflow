f99d55be4b7d89a99322c216a9f00f3f1b539051
Jack Snider <choiman2021@gmail.com>
2022-08-08 19:52:04 +0900

    add

--- workspace/stack_underflow/src/main/java/ufo/files/dao/FilesDaoImpl.java
+++ workspace/stack_underflow/src/main/java/ufo/files/dao/FilesDaoImpl.java
@@ -1,5 +1,49 @@
 package ufo.files.dao;
 
+import java.sql.SQLException;
+
+import com.ibatis.sqlmap.client.SqlMapClient;
+
+import ufo.util.SqlMapClientFactory;
+import ufo.vo.FilesVO;
+
 public class FilesDaoImpl implements IFilesDao{
+
+	private SqlMapClient smc;
+	
+	private static FilesDaoImpl dao;
+	
+	private FilesDaoImpl() {
+		smc = SqlMapClientFactory.getSqlMapClient();
+	}
+	
+	public static IFilesDao getInstance() {
+		if(dao == null) dao = new FilesDaoImpl();
+		return dao;
+	}
+	
+	@Override
+	public int insertFiles(FilesVO fvo) {
+		int cnt = 0;
+		try {
+			if(smc.insert("files.insertFiles", fvo) == null) cnt = 1;
+		} catch (SQLException e) {
+			cnt = 0;
+			e.printStackTrace();
+		}
+		return cnt;
+	}
+
+	@Override
+	public int getFileNumByMemId(String memId) {
+		int res = 0;
+		try {
+			res = (int)smc.queryForObject("files.getFileNumByMemId", memId);
+		} catch (Exception e) {
+			res = 0;
+			e.printStackTrace();
+		}
+		return res;
+	}
 
 }
--- workspace/stack_underflow/src/main/java/ufo/files/service/FilesServiceImpl.java
+++ workspace/stack_underflow/src/main/java/ufo/files/service/FilesServiceImpl.java
@@ -1,5 +1,32 @@
 package ufo.files.service;
 
-public class FilesServiceImpl implements IFilesService{
+import ufo.files.dao.FilesDaoImpl;
+import ufo.files.dao.IFilesDao;
+import ufo.vo.FilesVO;
 
+public class FilesServiceImpl implements IFilesService{
+	
+	private IFilesDao dao;
+	
+	private static IFilesService service;
+	
+	private FilesServiceImpl() {
+		dao = FilesDaoImpl.getInstance();
+	}
+	
+	public static IFilesService getInstance() {
+		if(service == null) service = new FilesServiceImpl();
+		return service;
+	}
+	
+	@Override
+	public int insertFiles(FilesVO fvo) {
+		return dao.insertFiles(fvo);
+	}
+
+	@Override
+	public int getFileNumByMemId(String memId) {
+		return dao.getFileNumByMemId(memId);
+	}
+	
 }
