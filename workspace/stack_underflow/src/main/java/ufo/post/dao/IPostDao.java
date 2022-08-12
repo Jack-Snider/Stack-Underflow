cb521545bf88b4bea8d38b1399158803dbef94bb
Jack Snider <choiman2021@gmail.com>
2022-08-12 18:42:43 +0900

    post bit

--- workspace/stack_underflow/src/main/java/ufo/post/dao/IPostDao.java
+++ workspace/stack_underflow/src/main/java/ufo/post/dao/IPostDao.java
@@ -1,5 +1,23 @@
 package ufo.post.dao;
 
+import ufo.vo.PostVO;
+
 public interface IPostDao {
 
+	/*
+	 
+	 	작성자 : Jack Snider
+	 
+	 */
+	
+	/**
+	 * 
+	 * 매개변수 : PostVO 객체
+	 * 
+	 * @param post
+	 * @return 삽입 성공시 : 1, 실패시 : 0
+	 */
+	public int insertPost(PostVO post);
+	
+	
 }
