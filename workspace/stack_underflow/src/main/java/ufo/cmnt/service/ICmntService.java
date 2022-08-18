068fac0741cb7e1ec000aa7979d9c9e0f086ee1e
Jack Snider <choiman2021@gmail.com>
2022-08-18 11:59:08 +0900

    pr test

--- workspace/stack_underflow/src/main/java/ufo/cmnt/service/ICmntService.java
+++ workspace/stack_underflow/src/main/java/ufo/cmnt/service/ICmntService.java
@@ -1,5 +1,7 @@
 package ufo.cmnt.service;
 
+import java.util.List;
+
 import ufo.vo.CmntVO;
 
 public interface ICmntService {
@@ -12,6 +14,13 @@ public interface ICmntService {
 	 * @return
 	 */
 	public int insertCmnt( CmntVO cmntVo );
+	
+	/**
+	 * 게시글 번호가 n인 댓글들 가져오기
+	 * @param postNum
+	 * @return
+	 */
+	public List<CmntVO> getCmnts( int postNum );
 	
 	/* Jack Snider 끝 */
 	
