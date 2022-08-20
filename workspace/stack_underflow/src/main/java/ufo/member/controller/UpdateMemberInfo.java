64dcc052673bc12fcddc53ce02d00ed4c531280a
hogyu <hogyu@DESKTOP-TP55J67>
2022-08-20 22:51:59 +0900

    MyPage - 회원탈퇴 기능 필요

--- workspace/stack_underflow/src/main/java/ufo/member/controller/UpdateMemberInfo.java
+++ workspace/stack_underflow/src/main/java/ufo/member/controller/UpdateMemberInfo.java
@@ -16,12 +16,42 @@ import ufo.member.service.IMemberService;
 import ufo.member.service.MemberServiceImpl;
 import ufo.vo.MemberVO;
 
-@WebServlet("/getMemberInfo.do")
-public class getMemberInfo extends HttpServlet {
+@WebServlet("/updateMemberInfo.do")
+public class UpdateMemberInfo extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
-       
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("application/json; charset=utf-8");
+		
+		HttpSession session = request.getSession();
+		
+		MemberVO vo = (MemberVO)session.getAttribute("Mem_vo");
+		
+		vo.setMem_nknm(request.getParameter("mem_nknm"));
+		vo.setMem_email(request.getParameter("mem_email"));
+		vo.setMem_addr1(request.getParameter("mem_addr1"));
+		vo.setMem_addr2(request.getParameter("mem_addr2"));
+		
+		IMemberService service = MemberServiceImpl.getInstance();
+		int cnt = service.updateMemberInfo(vo);
+		
+		if(cnt != 0) {
+			session.setAttribute("Mem_vo", vo);
+			Gson gson = new Gson();
+			PrintWriter out = response.getWriter();
+			String jsonData = null;
+			jsonData = gson.toJson(vo);
+			out.write(jsonData);
+			response.flushBuffer();
+		}else {
+			System.out.println("updateMemberInfo 실패");
+		}
+		
+		/*
 		Gson gson = new Gson();
 		response.setCharacterEncoding("utf-8");
 		response.setContentType("application/json; charset=utf-8");
@@ -33,6 +63,7 @@ public class getMemberInfo extends HttpServlet {
 		jsonData = gson.toJson(memberInfo);
 		out.write(jsonData);
 		response.flushBuffer();
+		*/
 	}
 
 }
